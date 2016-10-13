0f5f61e28fa1a6f3804798e3333e7c985f20cc56
Infinite-KG <skygodding@hotmail.com>
2016-10-12 20:58:17 -0400

    Added the other manager pages

---
+++ www/js/health.js
@@ -0,0 +1,8 @@
+'Use Strict';
+angular.module('App').controller('healthController', function ($scope, $state,$cordovaOauth, $localStorage, $location,$http,$ionicPopup, $firebaseObject, Auth, FURL, Utils) {
+  var ref = new Firebase(FURL);
+
+
+
+
+});
